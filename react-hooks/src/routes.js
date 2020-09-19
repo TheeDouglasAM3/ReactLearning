f06be4da007d4410db69bd48700867aba5de7680
Douglas Marcelino <douglas_champion@hotmail.com>
2020-09-19 20:27:20 -0300

    Add useReducer hook

--- react-hooks/src/routes.js
+++ react-hooks/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import CustomHooks from './pages/CustomHooks'
+import HookUseReducer from './pages/HookUseReducer'
 
 const routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/custom-hooks" component={CustomHooks} /> 
+        <Route path="/hook-use-reducer" component={HookUseReducer} /> 
       </Switch>    
     </BrowserRouter>
   )
